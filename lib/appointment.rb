1b50331ac56efe219f3499670b7c6f3a112cd81f
Derfler <rmderfler@gmail.com>
2014-08-17 15:55:13 -0700

    add scope to appointment class

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -2,4 +2,7 @@ class Appointment < ActiveRecord::Base
   belongs_to :doctor
   belongs_to :patient
 
+  scope :weeks_appointments, ->(date) { where("datetime >= ? AND datetime <= ?",
+        date, date+7.days).order(:datetime) }
+
 end
--- spec/appointment_spec.rb
+++ spec/appointment_spec.rb
@@ -7,4 +7,10 @@ describe Appointment do
     expect(appointment).to be_a  Appointment
   end
 
+   it 'returns all appointments for the current week' do
+    appointment = Appointment.create(:datetime => '2014-08-18 09:00', :doctor_id => 1, :patient_id => 1)
+    appointment2 = Appointment.create(:datetime => '2014-08-28 09:00', :doctor_id => 1, :patient_id => 1)
+    expect(Appointment.weeks_appointments(Date.today)).to eq [appointment]
+  end
+
 end
